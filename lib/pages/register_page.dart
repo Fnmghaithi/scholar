a5b97f859e179721ae87316a38fa4ae3133d4495
Faisal <fnmghaithi@gmail.com>
2023-01-22 19:21:14 +0400

    Finished from the login and register pages UI.

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:scholar_chat/pages/login_page.dart';
+import 'package:scholar_chat/widgets/custom_button.dart';
+import 'package:scholar_chat/widgets/custom_textfield.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Column(
+            children: <Widget>[
+              const Spacer(flex: 2),
+              Image.asset('assets/images/scholar.png'),
+              const Text(
+                'Scholar Chat',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 32,
+                  fontFamily: 'pacifico',
+                ),
+              ),
+              const Spacer(flex: 2),
+              Row(
+                children: const [
+                  Text(
+                    'REGISTER',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
+              CustomTextField(
+                hintText: 'Email',
+              ),
+              const SizedBox(height: 10),
+              CustomTextField(
+                hintText: 'Password',
+              ),
+              const SizedBox(height: 15),
+              CustomButton(
+                buttonText: 'REGISTER',
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'Already have an account?',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return const LoginPage();
+                          },
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'LOGIN',
+                      style: TextStyle(
+                        color: Color(0xFFC7EDE6),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(flex: 3),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  CustomButton({
+    Key? key,
+    required this.buttonText,
+  });
+
+  String buttonText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Center(
+        child: Text(buttonText!),
+      ),
+      width: double.infinity,
+      height: 60,
+    );
+  }
+}
