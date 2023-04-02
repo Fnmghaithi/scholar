b1c94e440649ab11de165b760a2b1b28c81f86d5
Faisal <fnmghaithi@gmail.com>
2023-04-02 23:09:55 +0400

    Completed LoginCubit.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:scholar_chat/constants.dart';
 import 'package:scholar_chat/firebase_options.dart';
 import 'package:scholar_chat/pages/chat_page.dart';
+import 'package:scholar_chat/pages/cubits/login_cubit/login_cubit.dart';
 import 'package:scholar_chat/pages/login_page.dart';
 import 'package:scholar_chat/pages/register_page.dart';
 
@@ -19,18 +22,21 @@ class ScholarChat extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        LoginPage.id: (context) {
-          return LoginPage();
+    return BlocProvider(
+      create: (context) => LoginCubit(),
+      child: MaterialApp(
+        routes: {
+          LoginPage.id: (context) {
+            return LoginPage();
+          },
+          RegisterPage.id: (context) => RegisterPage(),
+          ChatPage.id: (context) => ChatPage(),
         },
-        RegisterPage.id: (context) => RegisterPage(),
-        ChatPage.id: (context) => ChatPage(),
-      },
-      theme: ThemeData(
-        scaffoldBackgroundColor: kPrimaryColor,
+        theme: ThemeData(
+          scaffoldBackgroundColor: kPrimaryColor,
+        ),
+        initialRoute: 'LoginPage',
       ),
-      initialRoute: 'LoginPage',
     );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,21 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:scholar_chat/helper/show_snack_bar.dart';
 import 'package:scholar_chat/pages/chat_page.dart';
+import 'package:scholar_chat/pages/cubits/login_cubit/login_cubit.dart';
 import 'package:scholar_chat/pages/register_page.dart';
 import 'package:scholar_chat/widgets/custom_button.dart';
 import 'package:scholar_chat/widgets/custom_textfield.dart';
 
-class LoginPage extends StatefulWidget {
-  LoginPage({Key? key}) : super(key: key);
+class LoginPage extends StatelessWidget {
   static String id = 'LoginPage';
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
   String? email, password;
 
   GlobalKey<FormState> formKey = GlobalKey();
@@ -24,115 +19,101 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return ModalProgressHUD(
-      inAsyncCall: isLoading,
-      child: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: Form(
-              key: formKey,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/images/scholar.png'),
-                    const Text(
-                      'Scholar Chat',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 32,
-                        fontFamily: 'pacifico',
-                      ),
-                    ),
-                    Row(
-                      children: const [
-                        Text(
-                          'LOGIN',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 15),
-                    CustomTextFormField(
-                      hintText: 'Email',
-                      onChanged: (value) => email = value,
-                    ),
-                    const SizedBox(height: 10),
-                    CustomTextFormField(
-                      hintText: 'Password',
-                      onChanged: (value) => password = value,
-                    ),
-                    const SizedBox(height: 15),
-                    CustomButton(
-                      buttonText: 'LOGIN',
-                      onTap: () async {
-                        if (formKey.currentState!.validate()) {
-                          setState(() {
-                            isLoading = true;
-                          });
-                          try {
-                            await loginUser();
-                            Navigator.pushNamed(
-                              context,
-                              ChatPage.id,
-                              arguments: email,
-                            );
-                          } on FirebaseAuthException catch (e) {
-                            if (e.code == 'user-not-found') {
-                              showSnackBar(
-                                context,
-                                'No user found for that email.',
-                              );
-                            } else if (e.code == 'wrong-password') {
-                              showSnackBar(
-                                context,
-                                'Wrong password provided for that user.',
-                              );
-                            }
-                          } catch (e) {
-                            showSnackBar(
-                              context,
-                              e.toString(),
-                            );
-                          }
-                          setState(() {
-                            isLoading = false;
-                          });
-                        } else {}
-                      },
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        const Text(
-                          'Don\'t have an account?',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, RegisterPage.id);
-                          },
-                          child: const Text(
-                            'REGISTER',
+    return BlocConsumer<LoginCubit, LoginState>(
+      listener: (context, state) {
+        if (state is LoginLoading) {
+          isLoading = true;
+        } else if (state is LoginSuccess) {
+          Navigator.pushNamed(context, ChatPage.id);
+          isLoading = false;
+        } else if (state is LoginFailure) {
+          showSnackBar(context, state.errMessage);
+          isLoading = false;
+        }
+      },
+      builder: ((context, state) => ModalProgressHUD(
+            inAsyncCall: isLoading,
+            child: Scaffold(
+              body: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: Form(
+                    key: formKey,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: <Widget>[
+                          Image.asset('assets/images/scholar.png'),
+                          const Text(
+                            'Scholar Chat',
                             style: TextStyle(
-                              color: Color(0xFFC7EDE6),
+                              color: Colors.white,
+                              fontSize: 32,
+                              fontFamily: 'pacifico',
                             ),
                           ),
-                        ),
-                      ],
+                          Row(
+                            children: const [
+                              Text(
+                                'LOGIN',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 15),
+                          CustomTextFormField(
+                            hintText: 'Email',
+                            onChanged: (value) => email = value,
+                          ),
+                          const SizedBox(height: 10),
+                          CustomTextFormField(
+                            hintText: 'Password',
+                            onChanged: (value) => password = value,
+                          ),
+                          const SizedBox(height: 15),
+                          CustomButton(
+                            buttonText: 'LOGIN',
+                            onTap: () async {
+                              if (formKey.currentState!.validate()) {
+                                BlocProvider.of<LoginCubit>(context).loginUser(
+                                  email: email!,
+                                  password: password!,
+                                );
+                              } else {}
+                            },
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              const Text(
+                                'Don\'t have an account?',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pushNamed(context, RegisterPage.id);
+                                },
+                                child: const Text(
+                                  'REGISTER',
+                                  style: TextStyle(
+                                    color: Color(0xFFC7EDE6),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ),
-      ),
+          )),
     );
   }
 
